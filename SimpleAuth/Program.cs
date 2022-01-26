87a2a6164626dab5e2e09fedf6baa145a8f199f7
Emmanuel Allison <emmallison13@gmail.com>
2022-01-26 06:23:08 +0100

    Connecting the dots...

--- SimpleAuth/Program.cs
+++ SimpleAuth/Program.cs
@@ -1,11 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using System.Reflection;
+using SimpleAuth.Services.Abstractions;
+using SimpleAuth.Services;
+using SimpleAuth.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Service configuration.
+Assembly controllersAssembly = typeof(SimpleAuth.Presentation.AssemblyReference).Assembly;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserContext, UserContext>();
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{controllersAssembly.GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddControllers().AddApplicationPart(controllersAssembly);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Allow All", builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyMethod();
+        builder.AllowAnyHeader();
+    });
+});
+
+builder.Services.AddDbContext<UserContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["Db:ConnectionString"]);
+    options.EnableSensitiveDataLogging();
+});
+
+#endregion
 
 var app = builder.Build();
 
@@ -17,6 +50,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("Allow All");
 
 app.UseAuthorization();
 
