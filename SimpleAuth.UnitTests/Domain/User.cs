ba11cca445a35c30a84ac883b14c47ad06af7348
Emmanuel Allison <emmallison13@gmail.com>
2022-01-26 03:42:28 +0100

    Created unit test for a User

---
+++ SimpleAuth.UnitTests/Domain/User.cs
@@ -0,0 +1,56 @@
+﻿using entity = SimpleAuth.Domain.Entities;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SimpleAuth.UnitTests.Domain;
+
+internal class User
+{
+    [Test]
+    public void FirstNameRequired()
+    {
+        entity.User user = new()
+        {
+            LastName = "Allison",
+        };
+
+        Assert.IsTrue(Validate.ValidateEntity(user)
+            .Any(x => x.MemberNames.Contains("FirstName") && x.ErrorMessage.Contains("First name required.")));
+    }
+
+    [Test]
+    public void LastNameRequired()
+    {
+        entity.User user = new()
+        {
+            FirstName = "Emmanuel",
+        };
+
+        Assert.IsTrue(Validate.ValidateEntity(user)
+            .Any(x => x.MemberNames.Contains("LastName") && x.ErrorMessage.Contains("Last name required.")));
+    }
+
+    [Test]
+    public void EmailRequired()
+    {
+        entity.User user = new()
+        {
+            LastName = "Allison",
+        };
+
+        Assert.IsTrue(Validate.ValidateEntity(user)
+            .Any(x => x.MemberNames.Contains("Email") && x.ErrorMessage.Contains("Email required.")));
+    }
+
+    [Test]
+    public void PasswordHashRequired()
+    {
+        entity.User user = new()
+        {
+            LastName = "Allison",
+        };
+
+        Assert.IsTrue(Validate.ValidateEntity(user)
+            .Any(x => x.MemberNames.Contains("PasswordHash") && x.ErrorMessage.Contains("Password required.")));
+    }
+}
---
+++ SimpleAuth.UnitTests/Domain/Validate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleAuth.UnitTests.Domain;
+
+internal static class Validate
+{
+    internal static IList<ValidationResult> ValidateEntity(object entity)
+    {
+        var validationResults = new List<ValidationResult>();
+        var context = new ValidationContext(entity, null, null);
+        Validator.TryValidateObject(entity, context, validationResults, true);
+        return validationResults;
+    }
+}
